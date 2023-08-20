ea89d711c2e604ba0e4803f6f2d6ed1ad4eea4bd
Ben <ben.larson@hotmail.com>
2023-08-20 16:59:00 +0100

    Remove all auth-related stuff as I think that's done

--- src/urls.rs
+++ src/urls.rs
@@ -1,22 +1,14 @@
-use std::f32::consts::E;
-
 const BASE_URL: &str = "https://eapi.pcloud.com";
 
 pub enum EndPoint {
-    GetDigest,
     ListFolder,
-    Oauth2Token,
-    UserInfo,
 }
 
 impl EndPoint {
     pub fn get_url(&self) -> String {
         let token = std::env::var("PHOTOFRAME_OAUTH_TOKEN").unwrap();
         let method_name = match self {
-            EndPoint::GetDigest => "getdigest",
             EndPoint::ListFolder => "listfolder",
-            EndPoint::Oauth2Token => "oauth2_token",
-            EndPoint::UserInfo => "userinfo",
         };
         format!("{}/{}?access_token={}", BASE_URL, method_name, token)
     }
